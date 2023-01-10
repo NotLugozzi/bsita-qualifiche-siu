5b9765a4329574bc577ebd4f8259eec87e866086
maiatteo <97740632+maiatteo@users.noreply.github.com>
2023-01-10 22:14:46 +0100

    Finished main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import json
-map_id = ["370710", "379722"]
+map_id = ["520163", "520247", "517385", "511498", "531963", "529093", "446433", "406385", "515782", "226550", "38016", "354669", "70185", "100024", "114296"]
 map_scores = []
+map_max_scores = []
 map_details_list = []
 
 def has_error_message(json_data):
@@ -18,7 +19,9 @@ for map in map_id:
         pizzi_dict = pizzi.json()
         scores = scores_dict['scores'][:10]
         map_scores.append(scores)
-        map_details_list.append(map_details.json())
+        map_json = map_details.json()
+        map_details_list.append(map_json)
+        map_max_scores.append(map_json["maxScore"])
         if has_error_message(pizzi_dict) == True:
             print("pizzi non trovato:nerd:")
         else:
@@ -43,6 +46,9 @@ html += "   table, th, td {\n"
 html += "   border: 2px solid black;\n"
 html += "   text-align: center;\n"
 html += "   }\n"
+html += "   table, p {\n"
+html += "   display: none;\n"
+html += "   }\n"
 html += "   h1, h2, h3{\n"
 html += "   text-align: center;\n"
 html += "   }\n"
@@ -52,22 +58,67 @@ html += "   margin-right: auto;\n"
 html += "   }\n"
 html += "</style>\n"
 html += '</head>\n'
-
-print(map_details_list)
-
+html += '<body>\n'
+html += "<div>\n"
+html += '  <button type="button" onclick="selectAcc()">Accuracy</button>\n'
+html += '  <button type="button" onclick="selectSpeed()">Speed</button>\n'
+html += '  <button type="button" onclick="selectTech()">Tech</button>\n'
+html += '  <button type="button" onclick="selectJoker()">Joker</button>\n'
+html += '  <button type="button" onclick="selectHist()">Historical</button>\n'
+html += '</div>'
+html += '<script> function selectAcc() { \n'
+html += '    const tables = document.getElementsByClassName("table");\n' 
+html += '    console.log(tables); for(var i = 0; i < tables.length; i++){\n' 
+html += '        tables[i].style.display = "none"};\n'
+html += '    const selected = document.getElementsByClassName("category0");\n' 
+html += '    for(var i = 0; i < selected.length; i++){\n' 
+html += '        selected[i].style.display = "table";\n'
+html += '        }}</script>\n'
+html += '<script> function selectSpeed() { \n'
+html += '    const tables = document.getElementsByClassName("table");\n' 
+html += '    console.log(tables); for(var i = 0; i < tables.length; i++){\n' 
+html += '        tables[i].style.display = "none"};\n'
+html += '    const selected = document.getElementsByClassName("category1");\n' 
+html += '    for(var i = 0; i < selected.length; i++){\n' 
+html += '        selected[i].style.display = "table";\n'
+html += '        }}</script>\n'
+html += '<script> function selectTech() { \n'
+html += '    const tables = document.getElementsByClassName("table");\n' 
+html += '    console.log(tables); for(var i = 0; i < tables.length; i++){\n' 
+html += '        tables[i].style.display = "none"};\n'
+html += '    const selected = document.getElementsByClassName("category2");\n' 
+html += '    for(var i = 0; i < selected.length; i++){\n' 
+html += '        selected[i].style.display = "table";\n'
+html += '        }}</script>\n'
+html += '<script> function selectJoker() { \n'
+html += '    const tables = document.getElementsByClassName("table");\n' 
+html += '    console.log(tables); for(var i = 0; i < tables.length; i++){\n' 
+html += '        tables[i].style.display = "none"};\n'
+html += '    const selected = document.getElementsByClassName("category3");\n' 
+html += '    for(var i = 0; i < selected.length; i++){\n' 
+html += '        selected[i].style.display = "table";\n'
+html += '        }}</script>\n'
+html += '<script> function selectHist() { \n'
+html += '    const tables = document.getElementsByClassName("table");\n' 
+html += '    console.log(tables); for(var i = 0; i < tables.length; i++){\n' 
+html += '        tables[i].style.display = "none"};\n'
+html += '    const selected = document.getElementsByClassName("category4");\n' 
+html += '    for(var i = 0; i < selected.length; i++){\n' 
+html += '        selected[i].style.display = "table";\n'
+html += '        }}</script>\n'
 for score in map_scores:
-    html += '<table class="table">\n'
+    max_score = map_max_scores[map_scores.index(score)]
+    category = (map_scores.index(score)) // 3
+    html += f'<table class="table category{category}">\n'
     html += '  <thead>\n'
-    html += '    <tr>\n'
-    html += '      <p>\n'
+    html += f'      <p class="table category{category}">\n'
     html += f'        {map_details_list[map_scores.index(score)]["songName"]} - {map_details_list[map_scores.index(score)]["songAuthorName"]}\n'
     html += '      </p>\n'
-    html += '    </tr>\n'
     html += '    <tr>\n'
     html += '      <th scope="col">#</th>\n'
     html += '      <th scope="col">Name</th>\n'
     html += '      <th scope="col">Score</th>\n'
-    html += '      <th scope="col">FC</th>\n'
+    html += '      <th scope="col">Miss</th>\n'
     html += '      <th scope="col">Punti</th>\n'
     html += '    </tr>\n'
     html += '  </thead>\n'
@@ -89,16 +140,19 @@ for score in map_scores:
             points = 10
         elif i == 5:
             points = 1
+        perc = format((item["baseScore"] / max_score) * 100, ".2f")
+        miss = item["badCuts"] + item["missedNotes"]
         html += f'    <tr class="{row_class}">\n'
         html += f'      <th scope="row">{i + 1}</th>\n'
         html += f'      <td>{item["leaderboardPlayerInfo"]["name"]}</td>\n'
-        html += f'      <td>{item["baseScore"]}</td>\n'
-        html += f'      <td>{item["fullCombo"]}</td>\n'
+        html += f'      <td>{perc}%</td>\n'
+        html += f'      <td>{miss}</td>\n'
         html += f'      <td>{points}</td>\n'
         html += '    </tr>\n'
 
 html += '  </tbody>\n'
 html += '</table>\n'
+html += '</body>\n'
 with open("index.html", "w", encoding="utf-16") as f:
     f.write(html)
     print("index.html scritto")
